7792ef79d861f1d61304a4e843b603066e0cc27b
Sergeii Samus <cergi8273@gmail.com>
2023-05-08 16:31:39 +0300

    feat:Added a solution for task number three

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,6 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'notes', 'mynotes.txt'), 'utf-8', (data) => {
-  console.log(data);
+const dirPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(dirPath, (err, files) => {
+  //   console.log(files);
+  files.forEach((file) => {
+    let fileNow = path.join(dirPath, file);
+
+    fs.stat(fileNow, (err, status) => {
+      if (status.isFile()) {
+        const { name, ext } = path.parse(file);
+        console.log(`${name} - ${ext.slice(1)}`);
+      } else {
+      }
+    });
+  });
 });
