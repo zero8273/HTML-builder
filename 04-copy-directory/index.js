6cef9fd1e23746e25b9700c69d085a23c9880423
Sergeii Samus <cergi8273@gmail.com>
2023-05-08 18:25:29 +0300

    feat: add solution for task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+const newDirPath = path.join(__dirname, 'files-copy');
+const dirPath = path.join(__dirname, 'files');
+
+fs.rm(newDirPath, { recursive: true }, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  fs.mkdir(newDirPath, () => {
+    fs.readdir(dirPath, (err, files) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      files.forEach((file) => {
+        let fileNow = path.join(dirPath, file);
+        fs.readFile(fileNow, 'utf-8', (err, data) => {
+          let newFilePath = path.join(newDirPath, file);
+          fs.writeFile(newFilePath, data, () => {});
+        });
+      });
+    });
+  });
+});
