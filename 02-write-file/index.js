7f9c8727814ce942044d6360b7670a9aa9949cde
Sergeii Samus <cergi8273@gmail.com>
2023-05-09 20:04:45 +0300

    fix: fix bug with ctrl+c close

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,24 +4,27 @@ const readline = require('readline');
 
 const filePath = path.join(__dirname, 'text.txt');
 
+process.on('SIGINT', async () => {
+  console.log('Прощай');
+  await line.close();
+  process.exit();
+});
+
 const line = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
+line.on('close', () => {
+  console.log('Прощайте');
+  process.exit();
+});
 fs.promises.writeFile(filePath, '').then(() => {
   console.log('Введите текст');
   line.on('line', async (input) => {
     if (input === 'exit') {
-      console.log('Прощайте');
-      rl.close();
+      line.close();
     } else {
       await fs.promises.appendFile(filePath, input + '\n');
     }
   });
-});
-
-process.on('SIGINT', () => {
-  console.log('Прощайте');
-  process.exit();
 });
